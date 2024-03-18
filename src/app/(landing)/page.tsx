ec79d399762e45b18d883aa09c774b16c6bceb35
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2024-03-18 09:26:28 +0200

    edit: homepage bg texture

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <section className="flex min-h-svh flex-col items-center justify-center p-5">
-      <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#71717A15_1px,transparent_1px),linear-gradient(to_bottom,#71717A15_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+      <div className="absolute top-0 -z-10 h-full w-full bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       <nav className="mb-16">
         <ul className="flex items-center justify-center gap-4">
