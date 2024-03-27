57a450eed6e0f8bacb7fbdd709e317bd1977e142
Simon Farah <88495690+simonfarah@users.noreply.github.com>
2024-03-27 14:45:06 +0200

    add: velite setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+class VeliteWebpackPlugin {
+  static started = false;
+  constructor(/** @type {import('velite').Options} */ options = {}) {
+    this.options = options;
+  }
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs !!!
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return;
+      VeliteWebpackPlugin.started = true;
+      const dev = compiler.options.mode === 'development';
+      this.options.watch = this.options.watch ?? dev;
+      this.options.clean = this.options.clean ?? !dev;
+      const { build } = await import('velite');
+      await build(this.options); // start velite
+    });
+  }
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
+    return config;
+  },
 };
 
 module.exports = nextConfig;
---
+++ velite.config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, defineConfig, s } from 'velite';
+
+const works = defineCollection({
+  name: 'Work',
+  pattern: '**/*.{md,mdx}',
+  schema: s.object({
+    slug: s.path(),
+    title: s.string(),
+    description: s.string(),
+    tags: s.array(s.string()),
+    content: s.mdx(),
+  }),
+});
+
+export default defineConfig({
+  root: 'src/content',
+  output: {
+    clean: true,
+  },
+  collections: { works },
+});
